4edf6f6c4e0a9abcf8f2a590990cb7f25468b328
sapronov1021 <sapronov1021@gmail.com>
2021-11-25 11:54:39 +0200

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const Header = () => (
+  <header>
+    <div>
+      <h1>sdf</h1>
+    </div>
+  </header>
+)
+
+export default Header
---
+++ src/components/layout.js
@@ -0,0 +1,24 @@
+import * as React from "react"
+import Header from "./header"
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <div>
+        <main>{children}</main>
+        <footer
+          style={{
+            marginTop: `2rem`,
+          }}
+        >
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </footer>
+      </div>
+    </>
+  )
+}
+
+export default Layout
