36565ed93956709f4763fc458f00e0247c5d23b1
sapronov1021 <sapronov1021@gmail.com>
2022-01-17 21:30:11 +0200

    Update testimonials

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -4,28 +4,30 @@ import StarRatings from "react-star-ratings"
 
 const TestimonialItem = ({ data }) => {
   return (
-    <div className="col-lg-4 col-md-6">
-      <div className="testimonial-item">
-        <img
-          className="testimonial-item__avatar"
-          src={data.avatar.localFile.url}
-          alt="avatar"
-        />
-        <img src={Closequote} alt="closequote" />
-        <p className="testimonial-item__name">{data.testimonialTitle}</p>
-        <p className="testimonial-item__role">{data.testimonialSubtitle}</p>
-        <div
-          className="testimonial-item__text"
-          dangerouslySetInnerHTML={{ __html: data.testimonialText }}
-        />
-        <StarRatings
-          rating={data.stars}
-          starDimension="30px"
-          starSpacing="2.5px"
-          starRatedColor=" #FFC107"
-        />
+    <div className="testimonial-item">
+        <div className="testimonial-item__avatar">
+          <img
+            src={data.avatar.localFile.url}
+            alt="avatar"
+          />
+        </div>
+        <div classNmae="testimonial-item-detail">
+            <div><img src={Closequote} alt="closequote" /></div>
+            <div
+              className="testimonial-item-detail__text"
+              dangerouslySetInnerHTML={{ __html: data.testimonialText }}
+            />
+            <span className="testimonial-item-detail__name">{data.testimonialTitle}</span>
+            <span className="testimonial-item-detail__role">{data.testimonialSubtitle}</span>
+            
+            {/* <StarRatings
+              rating={data.stars}
+              starDimension="30px"
+              starSpacing="2.5px"
+              starRatedColor=" #FFC107"
+            /> */}
+        </div>
       </div>
-    </div>
   )
 }
 
