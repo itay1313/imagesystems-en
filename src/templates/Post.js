c349b7b1b68710342d522d2356ef10cab30ce3ce
itay haephrati <itay1313@gmail.com>
2022-01-23 23:19:49 +0200

    page and post

---
+++ src/templates/Post.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/layout"
+const PostTemplate = () => (
+  <Layout>
+    <h1>Post Template</h1>
+  </Layout>
+)
+export default PostTemplate
