a03c020c87f06121c050641eccd7b4365cb29aad
itay haephrati <itay1313@gmail.com>
2022-01-24 15:36:03 +0200

    test

--- src/templates/Page.js
+++ src/templates/Page.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 const PageTemplate = () => (
   <Layout>
-    <h1>Post 1</h1>
+    <h1>Page Template</h1>
   </Layout>
 )
 export default PageTemplate
