9ed8eee982850a1f7380329b3a9a0c2c95665800
Yasin Junet <yasintze@gmail.com>
2022-01-30 21:55:09 +0700

    style: product key features section

--- src/templates/Page.js
+++ src/templates/Page.js
@@ -1,10 +1,65 @@
-import React from "react"
+import React, { useRef } from "react"
 import { graphql } from "gatsby"
+import Slider from "react-slick"
 
 import Layout from "../components/layout"
+import FeaturesTemplate from "../components/Features"
+import { LeftBtn, RightBtn } from "../utils/imgImport"
+
+const NewsArrowLeft = ({ ...props }) => (
+  <button
+    {...props}
+    className="custom-arrow left"
+    aria-hidden="true"
+    type="button"
+  >
+    <img src={LeftBtn} loading="lazy" alt="left arrow" />
+  </button>
+)
+
+const NewsArrowRight = ({ ...props }) => (
+  <button
+    {...props}
+    className="custom-arrow right"
+    aria-hidden="true"
+    type="button"
+  >
+    <img src={RightBtn} loading="lazy" alt="right arrow" />
+  </button>
+)
 
 const PageTemplate = ({ data }) => {
+  const slider = useRef()
   const { title, content, featuredImage, productQuery } = data.wpProduct
+
+  const features_settings = {
+    dots: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    appendDots: dots => <ul>{dots}</ul>,
+    customPaging: () => <div className="ft-slick__dots--custom"></div>,
+    prevArrow: <NewsArrowLeft />,
+    nextArrow: <NewsArrowRight />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  }
+
   return (
     <Layout>
       <div className="product-section">
@@ -102,6 +157,18 @@ const PageTemplate = ({ data }) => {
             </div>
           </div>
         </section>
+        <section className="product-section__even product-section__features">
+          <div className="container">
+            <h2>Key features</h2>
+            {
+              <Slider ref={c => (slider.current = c)} {...features_settings}>
+                {productQuery?.keyFeatures.map((item, index) => (
+                  <FeaturesTemplate data={item} key={index} />
+                ))}
+              </Slider>
+            }
+          </div>
+        </section>
       </div>
     </Layout>
   )
@@ -143,6 +210,12 @@ export const query = graphql`
         }
         keyFeatures {
           title
+          content
+          buttonName
+          buttonLink
+          img {
+            srcSet
+          }
         }
       }
     }
