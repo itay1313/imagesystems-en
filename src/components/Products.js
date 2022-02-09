eebba3921a7f960dd03bc4b49dd6d75c3415991d
Yasin Junet <yasintze@gmail.com>
2022-02-09 21:56:38 +0700

    style: fix learn more horizontally aligned

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,18 +4,18 @@ import React from "react"
 const ProductItem = ({ data }) => {
   return (
     <div className="product-item">
+      <img
+        loading="lazy"
+        src={data.featuredImage.node.sourceUrl}
+        alt="product"
+        className="product-item__img"
+      />
+      <h3 className="product-item__title mb-0">{data.title}</h3>
+      <div
+        className="product-item__content"
+        dangerouslySetInnerHTML={{ __html: data.excerpt }}
+      />
       <Link to={data.uri}>
-        <img
-          loading="lazy"
-          src={data.featuredImage.node.sourceUrl}
-          alt="product"
-          className="product-item__img"
-        />
-        <h3 className="product-item__title mb-0">{data.title}</h3>
-        <div
-          className="product-item__content"
-          dangerouslySetInnerHTML={{ __html: data.excerpt }}
-        />
         <div className="product-item__link">Learn more</div>
       </Link>
     </div>
